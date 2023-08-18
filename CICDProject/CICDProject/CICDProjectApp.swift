22859f40642aa9208c97f773a064cc7d19f36895
AyaBassi <pkarbassi@hotmail.co.uk>
2023-08-18 17:24:40 +0100

    First commit

---
+++ CICDProject/CICDProject/CICDProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDProjectApp.swift
+//  CICDProject
+//
+//  Created by Payam Karbassi on 18/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct CICDProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
